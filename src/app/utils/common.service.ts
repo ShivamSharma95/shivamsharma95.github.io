a721058458718143a19aff9d3a29cd5dc8229681
ShivamSharma95 <itsshivam95@gmail.com>
2025-11-10 09:54:24 +0000

    🚀 Auto-deployed build from main on Mon Nov 10 09:54:24 UTC 2025

--- src/app/utils/common.service.ts
+++
@@ -1,9 +0,0 @@
-import { ViewportScroller } from "@angular/common";
-
-export function scrollToSection(viewportScroller: ViewportScroller, anchor: string): void {
-    if (anchor) {
-        viewportScroller.scrollToAnchor(anchor);
-    } else {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-}
